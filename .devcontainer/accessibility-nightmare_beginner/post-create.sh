a7780e00c61f40dc51e2d07788ddaec7e89b79eb
Katharina Sick <katharina.sick@dynatrace.com>
2026-09-08 09:12:31 +0200

    move accessbility nightmare from planned to live
    Signed-off-by: Katharina Sick <katharina.sick@dynatrace.com>

--- .devcontainer/accessibility-nightmare_beginner/post-create.sh
+++ .devcontainer/accessibility-nightmare_beginner/post-create.sh
@@ -5,12 +5,12 @@ REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 
 # shellcheck disable=SC1091
 source "$REPO_ROOT/lib/scripts/tracker.sh"
-set_tracking_context "adventure-accessibility-nightmare" "beginner" "" "TODO" "TODO"
+set_tracking_context "accessibility-nightmare" "beginner" "07" "09" "2026"
 track_container_created
 
 "$REPO_ROOT/lib/shared/init.sh" --version v0.17.0
 
-CHALLENGE_DIR="$REPO_ROOT/adventures/planned/adventure-accessibility-nightmare/beginner"
+CHALLENGE_DIR="$REPO_ROOT/adventures/accessibility-nightmare/beginner"
 
 echo "✨ Installing ShopSmart dependencies..."
 cd "$CHALLENGE_DIR"
