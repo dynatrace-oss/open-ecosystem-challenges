526e6679b24276469ceaf7aa705d0c0709950ea0
Katharina Sick <katharina.sick@dynatrace.com>
2026-09-08 09:34:46 +0200

    fix accessiblity nightmare verification script after moving it
    Signed-off-by: Katharina Sick <katharina.sick@dynatrace.com>

--- adventures/accessibility-nightmare/beginner/verify.sh
+++ adventures/accessibility-nightmare/beginner/verify.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 # shellcheck disable=SC1091
-source "$SCRIPT_DIR/../../../../lib/scripts/loader.sh"
+source "$SCRIPT_DIR/../../../lib/scripts/loader.sh"
 
 set_tracking_context "accessibility-nightmare" "beginner" "07" "09" "2026"
 
